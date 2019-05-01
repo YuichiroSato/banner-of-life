6c2909e4081c4c0e08ee313529d24229519900d7
YuichiroSato <sato.yu1ro@gmail.com>
2019-05-01 23:57:36 +0900

    Implement wasm bind

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,15 @@
 extern crate cfg_if;
+extern crate rand;
 extern crate wasm_bindgen;
+extern crate web_sys;
 
+mod game_of_life;
 mod utils;
+mod cells;
 
+use wasm_bindgen::JsCast;
 use wasm_bindgen::prelude::*;
+use game_of_life::GameOfLife;
 
 cfg_if::cfg_if! {
     if #[cfg(feature = "wee_alloc")] {
@@ -13,11 +19,45 @@ cfg_if::cfg_if! {
 }
 
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+pub struct Banner {
+    canvas_id: String,
+    game_of_life: GameOfLife,
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, {{project-name}}!");
+impl Banner {
+    pub fn new(canvas_id: &str) -> Banner {
+        Banner {
+            canvas_id: canvas_id.to_string(),
+            game_of_life: GameOfLife::new(100, 30),
+        }
+    }
+
+    pub fn initialize(&mut self) {
+        self.game_of_life.randomize();
+    }
+
+    pub fn tick(&mut self) {
+        self.game_of_life.evolve();
+        self.draw();
+    }
+
+    pub fn draw(&self) {
+        let context = web_sys::window()
+            .unwrap()
+            .document()
+            .unwrap()
+            .get_element_by_id(&*self.canvas_id)
+            .unwrap()
+            .dyn_into::<web_sys::HtmlCanvasElement>()
+            .map_err(|_| ())
+            .unwrap()
+            .get_context("2d")
+            .unwrap()
+            .unwrap()
+            .dyn_into::<web_sys::CanvasRenderingContext2d>()
+            .unwrap();
+
+        self.game_of_life.draw(&context);
+    }
 }
