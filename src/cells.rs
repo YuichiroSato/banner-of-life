5f4a4bf973e6a163f5b8552e003be0a6ce2c3780
YuichiroSato <sato.yu1ro@gmail.com>
2019-05-05 22:41:42 +0900

    Delete Cells trait

--- src/cells.rs
+++ src/cells.rs
@@ -5,15 +5,6 @@ pub enum Cell {
     Alive = 1,
 }
 
-pub trait Cells {
-    fn new(size_x: usize, size_y: usize) -> Self;
-    fn replicate(&self) -> Self;
-    fn make_alive(&mut self, x: usize, y: usize);
-    fn make_dead(&mut self, x: usize, y: usize);
-    fn is_alive(&self, x: usize, y: usize) -> bool;
-    fn count_alive_around(&self, x: usize, y: usize) -> i32;
-}
-
 pub struct CellsImpl {
     pub size_x: usize,
     pub size_y: usize,
